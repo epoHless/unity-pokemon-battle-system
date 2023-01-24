8f3a720b5b7329d35c35c4466087f20bee7211a1
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-01-24 23:16:45 +0100

    updated moveblock

--- Assets/_Scripts/Pokemon/Moves/MoveBlocks/MoveBlock.cs
+++ Assets/_Scripts/Pokemon/Moves/MoveBlocks/MoveBlock.cs
@@ -1,8 +1,18 @@
 ﻿using System.Collections;
+using UnityEngine;
 
 [System.Serializable]
 public class MoveBlock
 {
+    #if UNITY_EDITOR
+    [HideInInspector] public string name;
+
+    public MoveBlock()
+    {
+        name = ToString();
+    }
+    #endif
+
     public virtual IEnumerator Execute(MoveSO moveData, Pokemon caster, Pokemon afflictedPokemon)
     {
         yield return null;
