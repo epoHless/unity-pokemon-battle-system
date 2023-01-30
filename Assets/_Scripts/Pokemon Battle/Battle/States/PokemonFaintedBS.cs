acc5d1d9eb97764af2508aff31139528f207e70d
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-01-30 18:48:10 +0100

    Added move

--- Assets/_Scripts/Pokemon Battle/Battle/States/PokemonFaintedBS.cs
+++ Assets/_Scripts/Pokemon Battle/Battle/States/PokemonFaintedBS.cs
@@ -4,17 +4,11 @@ public class PokemonFaintedBS : BattleState
     public override void OnEnter(BattleManager manager)
     {
         base.OnEnter(manager);
-        
-        
+
         PokemonBagManager.Instance.SetUI();
         
         UIManager.Instance.ToggleHUD(true);
         UIManager.Instance.TogglePokemons(true);
-    }
-
-    public override void OnUpdate(BattleManager manager)
-    {
-        base.OnUpdate(manager);
     }
 
     public override void OnExit(BattleManager manager)
